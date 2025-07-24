a657ee387a7c853538fdce07b77487476f441486
Rania Ali <ranianali426@gmail.com>
2025-07-24 09:43:23 -0400

    trying out more than one page

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("this is the homepage.")
+                NavigationLink(destination: SecondView ()) {
+                    Text("click me")
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ Navigation/SecondView.swift
@@ -0,0 +1,16 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct SecondView: View {
+    var body: some View {
+        Text("You've arrived to the second view!")
+    }
+}
